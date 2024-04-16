0746d645e2149a2ea28788eb56c2e04dce619845
Mouhamadou Kane <116074221+Mouhamadou305@users.noreply.github.com>
2024-04-16 00:09:21 +0000

    Ajoutez des fichiers projet.

---
+++ EhodVenteEnLigne/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,27 @@
+﻿using EhodBoutiqueEnLigne.Controllers;
+using Microsoft.Extensions.Localization;
+using System.ComponentModel.DataAnnotations;
+
+namespace EhodBoutiqueEnLigne.Models.ViewModels
+{
+    public class LoginModel
+    {
+        private readonly IStringLocalizer<LoginModel> _localizer;
+
+        public LoginModel(IStringLocalizer<LoginModel> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public LoginModel()
+        {        }
+
+        [Required(ErrorMessage ="MissingLoginName")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "MissingLoginPassword")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
---
+++ EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace EhodBoutiqueEnLigne.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "ErrorMissingName")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "MissingQuantity")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "StockNotANumber")]
+        [Range(minimum: 0, maximum: int.MaxValue, ErrorMessage = "StockNotGreaterThanZero")]
+        public string Stock { get; set; }
+
+        [Required(ErrorMessage = "MissingPrice")]
+        [RegularExpression("^\\d+(\\.\\d+)?$", ErrorMessage = "PriceNotANumber")]
+        [Range(minimum: 0, maximum: double.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
+        public string Price { get; set; }
+    }
+}
